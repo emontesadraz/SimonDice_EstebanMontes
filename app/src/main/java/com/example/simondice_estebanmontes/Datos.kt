98ca82d018228190b10b3655bcb918aca31f0425
estebanmontes <emontesadraz@danielcastelao.org>
2024-11-27 18:46:06 +0100

    Clase datos hecha

--- app/src/main/java/com/example/simondice_estebanmontes/Datos.kt
+++ app/src/main/java/com/example/simondice_estebanmontes/Datos.kt
@@ -1,4 +1,18 @@
 package com.example.simondice_estebanmontes
 
-object Datos {
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+
+data class Datos(var puntuacion: Int)
+
+class RepositorioPuntuacion {
+    private val _puntuacion = MutableStateFlow(Datos(0))
+    val puntuacion: Flow<Datos> get() = _puntuacion
+
+    /**
+     * Incrementa la puntuación
+     */
+    fun aumentarPuntuacion() {
+        _puntuacion.value = Datos(_puntuacion.value.puntuacion + 1)
+    }
 }
